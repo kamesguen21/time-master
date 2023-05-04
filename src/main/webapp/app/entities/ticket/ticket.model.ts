b6e98cbc17e3e24e5097514f6f038a1757d980ba
Your Name <medkhames.guen@satoripop.com>
2023-05-04 14:20:06 +0100

    Revert "jdl"
    This reverts commit 1fe65b4b8447f91678a58d0f287b75c1be0a568b.

--- src/main/webapp/app/entities/ticket/ticket.model.ts
+++
@@ -1,8 +0,0 @@
-export interface ITicket {
-  id: number;
-  key?: string | null;
-  summary?: string | null;
-  description?: string | null;
-}
-
-export type NewTicket = Omit<ITicket, 'id'> & { id: null };
